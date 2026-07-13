25b0705dc771bf980c19be9bb8fd2447992b752a
20233146 <kasparas.murmokas@stud.vilniustech.lt>
2026-07-13 20:33:44 +0300

    fix: remove not needed files

--- src/main/java/org/coderacer/backend/common/persistence/JpaAuditingConfig.java
+++
@@ -1,8 +0,0 @@
-package org.coderacer.backend.common.persistence;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing
-public class JpaAuditingConfig {}
