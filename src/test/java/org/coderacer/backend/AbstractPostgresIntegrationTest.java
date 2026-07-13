5de4a21682e7feb5f394dba3ce6ebee355b0911b
20233146 <kasparas.murmokas@stud.vilniustech.lt>
2026-07-13 16:05:43 +0300

    chore: change import ordering so spotless checks passes

--- src/test/java/org/coderacer/backend/AbstractPostgresIntegrationTest.java
+++ src/test/java/org/coderacer/backend/AbstractPostgresIntegrationTest.java
@@ -2,9 +2,9 @@ package org.coderacer.backend;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
 @SpringBootTest
 @Testcontainers
