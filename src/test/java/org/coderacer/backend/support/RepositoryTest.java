ea550f490cead685dbd5674a7e5ffef2edc647db
MYSERY-MACHINE\HP <damian.karalkevic@gmail.com>
2026-07-15 01:40:58 +0300

    fix(backend): cleaned up quality checks

--- src/test/java/org/coderacer/backend/support/RepositoryTest.java
+++ src/test/java/org/coderacer/backend/support/RepositoryTest.java
@@ -1,16 +1,15 @@
 package org.coderacer.backend.support;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import org.junit.jupiter.api.Tag;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
@@ -20,5 +19,4 @@ import java.lang.annotation.Target;
 @EnableJpaRepositories(basePackages = {"org.coderacer.backend", "org.coderacer.testfixtures"})
 @Import(PostgresTestContainersConfiguration.class)
 @Tag("integration")
-public @interface RepositoryTest {
-}
+public @interface RepositoryTest {}
