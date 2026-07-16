d59a9743e0f70354be9b629a897298fb65a33204
Adrjola <adrijus.kontenis17@gmail.com>
2026-07-16 16:44:41 +0300

    refactor: simplify email implementation

--- src/main/java/org/coderacer/backend/email/EmailDeliveryConfig.java
+++
@@ -1,8 +0,0 @@
-package org.coderacer.backend.email;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableConfigurationProperties(EmailDeliveryProperties.class)
-public class EmailDeliveryConfig {}
--- src/main/java/org/coderacer/backend/user/verification/config/EmailVerificationConfig.java
+++
@@ -1,8 +0,0 @@
-package org.coderacer.backend.user.verification.config;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableConfigurationProperties(EmailVerificationProperties.class)
-public class EmailVerificationConfig {}
